d6140bf43423325df44c7b2fd7c169411ca4d869
Daniel Wang <3644140+daniel-tron@users.noreply.github.com>
2022-01-07 15:11:00 +0800

    chore: update dashboard version

--- core/corehttp/webui.go
+++ core/corehttp/webui.go
@@ -1,10 +1,11 @@
 package corehttp
 
-const WebUIPath = "/btfs/QmRwxtQfzpfaLKYfg3qhxsFfpRm3J3qLXJxDofsLV8ydXq"
+const WebUIPath = "/btfs/QmRZYABH4LisEQPQmpzHAAxacDZbsKtnysjzEuZBaug6bG" // v2.0.1
 
 // this is a list of all past webUI paths.
 var WebUIPaths = []string{
 	WebUIPath,
+	"/btfs/QmRwxtQfzpfaLKYfg3qhxsFfpRm3J3qLXJxDofsLV8ydXq", // v2.0.0
 }
 
 var HostUIOption = RedirectOption("hostui", WebUIPath)
