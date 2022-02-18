c0db1fbb00e971ab75b68f885d276ea6a1116d09
matt <yrp1990@gmail.com>
2022-02-18 14:36:54 +0800

    feat: dashboard v2.1.0

--- core/corehttp/webui.go
+++ core/corehttp/webui.go
@@ -1,10 +1,11 @@
 package corehttp
 
-const WebUIPath = "/btfs/QmPSaMbVPTrcPg8CxW8GRrKYY9YZyxEQek9eKg5is13S9H" // v2.0.2
+const WebUIPath = "/btfs/QmVKKZuhYriR26jAkZGwR7jEPLGARtWZZV3oS2abykwT2U" // v2.1.0
 
 // this is a list of all past webUI paths.
 var WebUIPaths = []string{
 	WebUIPath,
+	"/btfs/QmPSaMbVPTrcPg8CxW8GRrKYY9YZyxEQek9eKg5is13S9H", // v2.0.2
 	"/btfs/QmRZYABH4LisEQPQmpzHAAxacDZbsKtnysjzEuZBaug6bG", // v2.0.1
 	"/btfs/QmRwxtQfzpfaLKYfg3qhxsFfpRm3J3qLXJxDofsLV8ydXq", // v2.0.0
 }
