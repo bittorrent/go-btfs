b5231adf05bd4db4467f57af64c1b070bea52edf
ken.sun <ken.sun@tron.network>
2022-02-25 14:55:38 +0800

    feat:mute sync hosts error log

--- spin/helper.go
+++ spin/helper.go
@@ -21,8 +21,6 @@ func periodicSync(period, timeout time.Duration, msg string, syncFunc func(conte
 			} else if strings.Contains(err.Error(), "Cannot query to get hosts") {
 				log.Errorf("Cannot query to get hosts. " +
 					"If you are not a renter, i.e. don't need to upload files, you can ignore it.")
-			} else {
-				log.Errorf("Failed to sync %s: %s", msg, err)
 			}
 		}
 	}
