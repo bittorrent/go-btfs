bcc48cb73f1b13acad6d1fa5ef6703dd81e5f41b
robin.luo <robin.luo@tron.network>
2022-03-07 16:12:42 +0800

    feat: unit tests for BTT/WBTT transfer and swap

--- settlement/swap/erc20/mock/erc20.go
+++ settlement/swap/erc20/mock/erc20.go
@@ -10,20 +10,46 @@ import (
 )
 
 type Service struct {
-	balanceOfFunc func(ctx context.Context, address common.Address) (*big.Int, error)
-	transferFunc  func(ctx context.Context, address common.Address, value *big.Int) (common.Hash, error)
+	addressFunc      func(ctx context.Context) common.Address
+	depositFunc      func(ctx context.Context, value *big.Int) (common.Hash, error)
+	withdrawFunc     func(ctx context.Context, value *big.Int) (common.Hash, error)
+	balanceOfFunc    func(ctx context.Context, address common.Address) (*big.Int, error)
+	transferFunc     func(ctx context.Context, address common.Address, value *big.Int) (common.Hash, error)
+	allowanceFunc    func(ctx context.Context, issuer common.Address, vault common.Address) (*big.Int, error)
+	approveFunc      func(ctx context.Context, address common.Address, value *big.Int) (common.Hash, error)
+	transferFromFunc func(ctx context.Context, issuer common.Address, vault common.Address, value *big.Int) (common.Hash, error)
+}
+
+func WithAddressFunc(f func(ctx context.Context) common.Address) Option {
+	return optionFunc(func(s *Service) { s.addressFunc = f })
+}
+
+func WithDepositFunc(f func(ctx context.Context, value *big.Int) (common.Hash, error)) Option {
+	return optionFunc(func(s *Service) { s.depositFunc = f })
+}
+
+func WithWithdrawFunc(f func(ctx context.Context, value *big.Int) (common.Hash, error)) Option {
+	return optionFunc(func(s *Service) { s.withdrawFunc = f })
+}
+
+func WithAllowanceFunc(f func(ctx context.Context, issuer common.Address, vault common.Address) (*big.Int, error)) Option {
+	return optionFunc(func(s *Service) { s.allowanceFunc = f })
+}
+
+func WithApproveFunc(f func(ctx context.Context, address common.Address, value *big.Int) (common.Hash, error)) Option {
+	return optionFunc(func(s *Service) { s.approveFunc = f })
+}
+
+func WithTransferFromFunc(f func(ctx context.Context, issuer common.Address, vault common.Address, value *big.Int) (common.Hash, error)) Option {
+	return optionFunc(func(s *Service) { s.transferFromFunc = f })
 }
 
 func WithBalanceOfFunc(f func(ctx context.Context, address common.Address) (*big.Int, error)) Option {
-	return optionFunc(func(s *Service) {
-		s.balanceOfFunc = f
-	})
+	return optionFunc(func(s *Service) { s.balanceOfFunc = f })
 }
 
 func WithTransferFunc(f func(ctx context.Context, address common.Address, value *big.Int) (common.Hash, error)) Option {
-	return optionFunc(func(s *Service) {
-		s.transferFunc = f
-	})
+	return optionFunc(func(s *Service) { s.transferFunc = f })
 }
 
 func New(opts ...Option) erc20.Service {
@@ -32,6 +58,48 @@ func New(opts ...Option) erc20.Service {
 		o.apply(mock)
 	}
 	return mock
+}
+
+func (s *Service) Address(ctx context.Context) common.Address {
+	if s.addressFunc != nil {
+		return s.addressFunc(ctx)
+	}
+	return common.Address{}
+}
+
+func (s *Service) Deposit(ctx context.Context, value *big.Int) (common.Hash, error) {
+	if s.depositFunc != nil {
+		return s.depositFunc(ctx, value)
+	}
+	return common.Hash{}, errors.New("error")
+}
+
+func (s *Service) Withdraw(ctx context.Context, value *big.Int) (common.Hash, error) {
+	if s.withdrawFunc != nil {
+		return s.withdrawFunc(ctx, value)
+	}
+	return common.Hash{}, errors.New("Error")
+}
+
+func (s *Service) Allowance(ctx context.Context, issuer common.Address, vault common.Address) (*big.Int, error) {
+	if s.allowanceFunc != nil {
+		return s.allowanceFunc(ctx, issuer, vault)
+	}
+	return big.NewInt(0), errors.New("Error")
+}
+
+func (s *Service) Approve(ctx context.Context, address common.Address, value *big.Int) (common.Hash, error) {
+	if s.approveFunc != nil {
+		return s.approveFunc(ctx, address, value)
+	}
+	return common.Hash{}, errors.New("Error")
+}
+
+func (s *Service) TransferFrom(ctx context.Context, issuer common.Address, vault common.Address, value *big.Int) (common.Hash, error) {
+	if s.transferFromFunc != nil {
+		return s.transferFromFunc(ctx, issuer, vault, value)
+	}
+	return common.Hash{}, errors.New("Error")
 }
 
 func (s *Service) BalanceOf(ctx context.Context, address common.Address) (*big.Int, error) {
