a1e682187bb1bf8671ccd675466427dc378dc93d
matt <yrp1990@gmail.com>
2022-03-30 15:20:50 +0800

    perf: improve heartbeat reporting
    improve heartbeat reporting

--- spin/analytics.go
+++ spin/analytics.go
@@ -40,8 +40,8 @@ var (
 
 // other constants
 const (
-	// HeartBeat is how often we send data to server, at the moment set to 15 Minutes
-	heartBeat = 15 * time.Minute
+	// HeartBeat is how often we send data to server, at the moment set to 60 Minutes
+	heartBeat = 60 * time.Minute
 
 	// Expotentially delayed retries will be capped at this total time
 	maxRetryTotal = 10 * time.Minute
