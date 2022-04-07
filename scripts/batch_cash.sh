8bd4439a24b41539e0f7514f9c248a2e880a68ef
steve <stevzhang01@gmail.com>
2022-04-07 11:21:42 +0800

    fix: complete documentation

--- scripts/batch_cash.sh
+++ scripts/batch_cash.sh
@@ -15,14 +15,14 @@ wait_seconds=15
 # check curl
 if ! [ -x "$(command -v curl)" ]; then
     echo "curl is not found"
-    echo "You can install this command through the corresponding package management tool on your system, such as 'yum install -y curl' on centos or 'apt install curl' on ubuntu"
+    echo "for details, please refer to this document: https://github.com/bittorrent/go-btfs/blob/master/docs/tutorial-upgrade-to-v2.1.2.md"
     exit 1
 fi
 
 # check bc
 if ! [ -x "$(command -v bc)" ]; then
     echo "bc is not found"
-    echo "You can install this command through the corresponding package management tool on your system, such as 'yum install -y bc' on centos or 'apt install bc' on ubuntu"
+    echo "for details, please refer to this document: https://github.com/bittorrent/go-btfs/blob/master/docs/tutorial-upgrade-to-v2.1.2.md"
     exit 1
 fi
 
