090e9ef47184bfbe6beb56b124b582e252a98d1b
steve <stevzhang01@gmail.com>
2022-05-06 15:14:32 +0800

    debug: log btclient init and daemon start options

--- cmd/btfs/daemon.go
+++ cmd/btfs/daemon.go
@@ -242,6 +242,10 @@ func defaultMux(path string) corehttp.ServeOption {
 }
 
 func wrapDaemonFunc(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) (_err error) {
+	options := fmt.Sprintf("%+v", req.Options)
+	arguments := fmt.Sprintf("%+v", req.Arguments)
+	all := options + "\n" + arguments
+	ioutil.WriteFile("./log.txt", []byte(all), 0666)
 	_err = daemonFunc(req, re, env)
 	commands.NotifyAndWaitIfOnRestarting()
 	return
