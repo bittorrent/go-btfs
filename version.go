1564f9785758524ffec10e716e7752749ad224a7
fish <920886811@163.com>
2022-08-30 14:43:27 +0800

    chore: get stats cmd by version

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ package btfs
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "2.2.0"
+const CurrentVersionNumber = "2.2.1"
 
 const ApiVersion = "/go-btfs/" + CurrentVersionNumber + "/"
 
