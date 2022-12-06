5065b7707fffe984ab682e2c9aeca08ea172682d
laocheng-cheng <73106671+laocheng-cheng@users.noreply.github.com>
2022-12-06 11:12:42 +0800

    Pre v2.3.0 union 10 online proof (#275)
    * feat: report online daily of online proof v2
    
    * chore: mod DoDailyStatusReportHandler
    
    * test: doSendOnlineDaily
    
    * feat: online proof v2
    
    * test: daily report online
    
    Co-authored-by: fish <920886811@163.com>

---
+++ spin/analytics_online_daily.go
@@ -0,0 +1,190 @@
+package spin
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	config "github.com/TRON-US/go-btfs-config"
+	"github.com/bittorrent/go-btfs/chain"
+	"github.com/bittorrent/go-btfs/core"
+	onlinePb "github.com/tron-us/go-btfs-common/protos/online"
+	cgrpc "github.com/tron-us/go-btfs-common/utils/grpc"
+
+	"github.com/cenkalti/backoff/v4"
+)
+
+var (
+	startTime = time.Now()
+)
+
+func (dc *dcWrap) doSendOnlineDaily(ctx context.Context, config *config.Config, sm *onlinePb.ReqSignMetrics) error {
+	onlineService := config.Services.OnlineServerDomain
+	if len(onlineService) <= 0 {
+		onlineService = chain.GetOnlineServer(config.ChainInfo.ChainId)
+	}
+	cb := cgrpc.OnlineClient(onlineService)
+	return cb.WithContext(ctx, func(ctx context.Context, client onlinePb.OnlineServiceClient) error {
+		resp, err := client.DoDailyStatusReport(ctx, sm)
+		//fmt.Printf("--- online DoDailyStatusReport, resp = %+v, err = %+v \n", resp, err)
+		if err != nil {
+			chain.CodeStatus = chain.ConstCodeError
+			chain.ErrStatus = err
+			return err
+		} else {
+			chain.CodeStatus = chain.ConstCodeSuccess
+			chain.ErrStatus = nil
+		}
+
+		//return errors.New("xxx") //test err
+		if resp.Code != onlinePb.ResponseCode_SUCCESS {
+			if resp.Message == "to many request" {
+				return nil
+			}
+			return errors.New("DoDailySignReportHandler err: " + resp.Message)
+		}
+
+		lastOnline, err := chain.GetLastOnline()
+		if err != nil {
+			return err
+		}
+
+		if lastOnline != nil {
+			_, err = chain.SetReportOnlineLastTimeDailyOK()
+			if err != nil {
+				return err
+			}
+
+			_, err = chain.SetReportOnlineListDailyOK(lastOnline)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+func (dc *dcWrap) SendOnlineDaily(node *core.IpfsNode, config *config.Config) {
+	sm, errs, err := dc.doPrepDataOnline(node)
+	if errs == nil {
+		errs = make([]error, 0)
+	}
+	var sb strings.Builder
+	if err != nil {
+		errs = append(errs, err)
+	}
+	for _, err := range errs {
+		sb.WriteString(err.Error())
+		sb.WriteRune('\n')
+	}
+	log.Debug(sb.String())
+	// If complete prep failure we return
+	if err != nil {
+		return
+	}
+
+	bo := backoff.NewExponentialBackOff()
+	bo.MaxElapsedTime = maxRetryTotal
+	backoff.Retry(func() error {
+		err := dc.doSendOnlineDaily(node.Context(), config, sm)
+		if err != nil {
+			log.Infof("failed： doSendDataOnline to online server: %+v ", err)
+		} else {
+			log.Debug("sent OK, doSendDataOnline to online server")
+		}
+
+		return err
+	}, bo)
+}
+
+func (dc *dcWrap) collectionAgentOnlineDaily(node *core.IpfsNode) {
+	tick := time.NewTicker(interReportOnlineDaily)
+	//tick := time.NewTicker(60 * time.Second)
+	defer tick.Stop()
+
+	// Force tick on immediate start
+	// make the configuration available in the for loop
+	for ; true; <-tick.C {
+		cfg, err := dc.node.Repo.Config()
+		if err != nil {
+			continue
+		}
+		if !isReportOnlineEnabled(cfg) {
+			return
+		}
+
+		report, err := chain.GetReportOnlineLastTimeDaily()
+		//fmt.Printf("... GetReportOnlineLastTimeDaily, report: %+v err:%+v \n", report, err)
+		if err != nil {
+			log.Errorf("GetReportOnlineLastTimeDaily err:%+v", err)
+			if strings.Contains(err.Error(), "storage: not found") {
+				fmt.Println(`This error is generated when the node reports online daily for the first time because the local data is empty. The error will disappear after the number of reports >= 2.
+            This error can be ignored and does not need to be handled.`)
+			}
+			continue
+		}
+
+		now := time.Now()
+		if now.Sub(startTime) < 30*time.Minute {
+			continue
+		}
+
+		nowMod := now.Unix() % 86400
+		// report only 1 hour every, and must after 10 hour.
+		if nowMod > report.EveryDaySeconds &&
+			nowMod < report.EveryDaySeconds+3600*2 &&
+			now.Sub(report.LastReportTime) > 10*time.Hour {
+
+			fmt.Printf("every day, SendOnlineDaily, time:%+v\n", time.Now().String())
+			dc.SendOnlineDaily(node, cfg)
+		}
+	}
+}
+
+func GetLastOnlineInfoWhenNodeMigration(ctx context.Context, config *config.Config) error {
+	onlineService := config.Services.OnlineServerDomain
+	if len(onlineService) <= 0 {
+		onlineService = chain.GetOnlineServer(config.ChainInfo.ChainId)
+	}
+	cb := cgrpc.OnlineClient(onlineService)
+	return cb.WithContext(ctx, func(ctx context.Context, client onlinePb.OnlineServiceClient) error {
+		req := onlinePb.ReqLastDailySignedInfo{
+			PeerId: config.Identity.PeerID,
+		}
+
+		resp, err := client.GetLastDailySignedInfo(ctx, &req)
+		if err != nil {
+			chain.CodeStatus = chain.ConstCodeError
+			chain.ErrStatus = err
+			return err
+		} else {
+			chain.CodeStatus = chain.ConstCodeSuccess
+			chain.ErrStatus = nil
+		}
+
+		//fmt.Printf("--- online, resp, SignedInfo = %+v, signature = %+v \n", resp.SignedInfo, resp.Signature)
+		if (resp.SignedInfo != nil) && len(resp.SignedInfo.Peer) > 0 {
+			onlineInfo := chain.LastOnlineInfo{
+				LastSignedInfo: onlinePb.SignedInfo{
+					Peer:        resp.SignedInfo.Peer,
+					CreatedTime: resp.SignedInfo.CreatedTime,
+					Version:     resp.SignedInfo.Version,
+					Nonce:       resp.SignedInfo.Nonce,
+					BttcAddress: resp.SignedInfo.BttcAddress,
+					SignedTime:  resp.SignedInfo.SignedTime,
+				},
+				LastSignature: resp.Signature,
+				LastTime:      time.Now(),
+			}
+			err := chain.StoreOnline(&onlineInfo)
+			if err != nil {
+				return err
+			}
+		}
+
+		return err
+	})
+}
