13ec81304cda80182f579a0956e85034f58a1c62
fish <920886811@163.com>
2022-12-12 14:39:13 +0800

    chore: mod version 2.3.0

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ package btfs
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "2.2.1.1"
+const CurrentVersionNumber = "2.3.0"
 
 const ApiVersion = "/go-btfs/" + CurrentVersionNumber + "/"
 
