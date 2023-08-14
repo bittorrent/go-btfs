7aa2ca5283bef70fbcce04e38ea444ef4fd46180
Steve <stevzhang01@gmail.com>
2023-08-14 17:22:01 +0800

    feat: add multiple context lock

--- s3/handlers/options.go
+++ s3/handlers/options.go
@@ -1,9 +1,3 @@
 package handlers
 
 type Option func(handlers *Handlers)
-
-func WithCorsAllowOrigins(origins []string) Option {
-	return func(handlers *Handlers) {
-		handlers.corsAllowOrigins = origins
-	}
-}
