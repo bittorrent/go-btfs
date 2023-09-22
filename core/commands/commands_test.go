71f6bccdaccffd2e6320f0365cb32b961c84f024
fish <920886811@163.com>
2023-09-22 19:39:57 +0800

    chore:

--- core/commands/commands_test.go
+++ core/commands/commands_test.go
@@ -349,15 +349,6 @@ func TestCommands(t *testing.T) {
 		"/multibase/list",
 		"/backup",
 		"/recovery",
-		"/accesskey/delete",
-		"/cheque/fix_cheque_cashout",
-		"/accesskey/list",
-		"/accesskey/get",
-		"/accesskey/enable",
-		"/accesskey/disable",
-		"/accesskey/reset",
-		"/accesskey",
-		"/accesskey/generate",
 	}
 
 	cmdSet := make(map[string]struct{})
