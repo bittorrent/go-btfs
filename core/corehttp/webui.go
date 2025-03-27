b1a49f0602d63a50b56b4a5feccaef0b723aa46c
cody <cody.meng@tron.network>
2025-03-27 10:31:32 +0800

    chore: format code.

--- core/corehttp/webui.go
+++ core/corehttp/webui.go
@@ -5,7 +5,7 @@ const WebUIPath = "/btfs/Qmcbn8vGxqM5bgYe386yh23khonzA6M56bmUyeqsm8heKo" // v3.3
 // this is a list of all past webUI paths.
 var WebUIPaths = []string{
 	WebUIPath,
-    "/btfs/QmNytdpG1FstSmR7eo547A8o9EdF7cFe1tcExunu8uEgDc", // v3.2.0
+	"/btfs/QmNytdpG1FstSmR7eo547A8o9EdF7cFe1tcExunu8uEgDc", // v3.2.0
 	"/btfs/QmXVrEfPAHqSSZp1hSMFh933WdxSzvbinSsYgV5bSWg5AE", // v3.1.0
 	"/btfs/QmRwt94fr6b3phJThuDMjZUXr6nHxxG61znN3PCukNAxyi", // v3.0.0
 	"/btfs/QmSTcj1pWk972bdtStQtJeu3yCYo1SDShrbEHaEybciTLR", // v2.3.5
