27244486480226ab4dce25d6f6b5936233dd1f95
cody <cody.meng@tron.network>
2025-06-06 10:54:24 +0800

    refactor(storage): update repair command to use chain metadata

--- core/commands/storage/upload/upload/repair.go
+++ core/commands/storage/upload/upload/repair.go
@@ -1,26 +1,22 @@
 package upload
 
 import (
-	"context"
 	"errors"
 	"strings"
 	"time"
 
+	"github.com/bittorrent/go-btfs/chain"
 	"github.com/bittorrent/go-btfs/chain/tokencfg"
+	"github.com/bittorrent/go-btfs/protos/metadata"
 	"github.com/bittorrent/go-btfs/utils"
 	"github.com/libp2p/go-libp2p/core/peer"
 
-	"github.com/bittorrent/go-btfs/core/commands/storage/helper"
 	uh "github.com/bittorrent/go-btfs/core/commands/storage/upload/helper"
 	"github.com/bittorrent/go-btfs/core/commands/storage/upload/sessions"
 
 	cmds "github.com/bittorrent/go-btfs-cmds"
-	"github.com/bittorrent/go-btfs-common/crypto"
-	guardpb "github.com/bittorrent/go-btfs-common/protos/guard"
-	"github.com/bittorrent/go-btfs-common/utils/grpc"
 )
 
-// TODO repair相关的命令要不要删除掉，这个是在guard服务里通过定时任务调用的
 var StorageUploadRepairCmd = &cmds.Command{
 	Helptext: cmds.HelpText{
 		Tagline: "Repair specific shards of a file.",
@@ -45,42 +41,29 @@ This command repairs the given shards of a file.`,
 			return err
 		}
 		fileHash := req.Arguments[0]
-		metaReq := &guardpb.CheckFileStoreMetaRequest{
-			FileHash:     fileHash,
-			RenterPid:    ctxParams.N.Identity.String(),
-			RequesterPid: ctxParams.N.Identity.String(),
-			RequestTime:  time.Now().UTC(),
-		}
-		sig, err := crypto.Sign(ctxParams.N.PrivateKey, metaReq)
 		if err != nil {
 			return err
 		}
-		metaReq.Signature = sig
-		ctx, _ := helper.NewGoContext(req.Context)
-		var meta *guardpb.FileStoreStatus
-		err = grpc.GuardClient(ctxParams.Cfg.Services.GuardDomain).WithContext(ctx, func(ctx context.Context,
-			client guardpb.GuardServiceClient) error {
-			// TODO 调整为调用合约
-			meta, err = client.CheckFileStoreMeta(ctx, metaReq)
-			if err != nil {
-				return err
-			}
-			return nil
-		})
+
+		var meta *metadata.FileMetaInfo
+		meta, err = chain.SettleObject.FileMetaService.GetFileMeta(fileHash, []string{})
 		if err != nil {
 			return err
 		}
-		contracts := meta.Contracts
+		if err != nil {
+			return err
+		}
+		contracts := meta.Agreements
 		if len(contracts) <= 0 {
 			return errors.New("length of contracts is 0")
 		}
-		ssId, _ := uh.SplitContractId(contracts[0].ContractId)
+		ssId, _ := uh.SplitContractId(contracts[0].Meta.AgreementId)
 		shardIndexes := make([]int, 0)
 		i := 0
 		shardHashes := strings.Split(req.Arguments[1], ",")
 		for _, contract := range contracts {
-			if contract.ShardHash == shardHashes[i] {
-				shardIndexes = append(shardIndexes, int(contract.ShardIndex))
+			if contract.Meta.ShardHash == shardHashes[i] {
+				shardIndexes = append(shardIndexes, int(contract.Meta.ShardIndex))
 				i++
 			}
 		}
@@ -89,7 +72,7 @@ This command repairs the given shards of a file.`,
 			return err
 		}
 		hp := uh.GetSPsProvider(ctxParams, strings.Split(req.Arguments[3], ","))
-		m := contracts[0].ContractMeta
+		m := contracts[0].Meta
 		renterPid, err := peer.Decode(req.Arguments[2])
 		if err != nil {
 			return err
@@ -99,14 +82,14 @@ This command repairs the given shards of a file.`,
 		UploadShard(&ShardUploadContext{
 			Rss:           rss,
 			HostsProvider: hp,
-			Price:         m.Price,
+			Price:         int64(m.Price),
 			Token:         tokencfg.GetWbttToken(),
-			ShardSize:     m.ShardFileSize,
+			ShardSize:     int64(m.ShardSize),
 			StorageLength: -1,
 			ShardIndexes:  shardIndexes,
 			RepairParams: &RepairParams{
-				RenterStart: m.RentStart,
-				RenterEnd:   m.RentEnd,
+				RenterStart: time.Unix(int64(m.StorageStart), 0),
+				RenterEnd:   time.Unix(int64(m.StorageEnd), 0),
 			},
 			RenterId: renterPid,
 		})
