5319781fdfa2bea3b0e7072b27917150a5e52dda
cody <cody.meng@tron.network>
2025-06-18 08:24:03 +0800

    fix: revert version number to 4.0.0

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ package btfs
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "4.4.0"
+const CurrentVersionNumber = "4.0.0"
 
 const ApiVersion = "/go-btfs/" + CurrentVersionNumber + "/"
 
